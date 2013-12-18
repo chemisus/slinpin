16e3557d964ba48db19fb3118cca6c677e848406
Terrence Howard <chemisus@gmail.com>
2013-12-17 19:11:57 -0500

    commenting

--- src/Slinpin/Slinpin.php
+++ src/Slinpin/Slinpin.php
@@ -4,19 +4,42 @@ namespace Slinpin;
 
 class Slinpin implements InjectableDependencyContainer
 {
+    /**
+     * @var Provider[]
+     */
     private $providers = [];
+
+    /**
+     * @var ReflectionContainer
+     */
     private $reflection_container;
 
+    /**
+     * @param ReflectionContainer $reflection_container
+     */
     public function __construct(ReflectionContainer $reflection_container)
     {
         $this->reflection_container = $reflection_container;
     }
 
+    /**
+     * Sets a provider.
+     *
+     * @param string $key
+     * @param Provider $value
+     */
     public function set($key, Provider $value)
     {
         $this->providers[$key] = $value;
     }
 
+    /**
+     * Gets a provider.
+     *
+     * @param string $key
+     * @return Provider
+     * @throws DependencyDoesNotExistException
+     */
     public function get($key)
     {
         if (!isset($this->providers[$key])) {
@@ -26,11 +49,23 @@ class Slinpin implements InjectableDependencyContainer
         return $this->providers[$key];
     }
 
+    /**
+     * Finds the provider, calls the providers provide, then returns the result.
+     *
+     * @param string $key
+     * @return mixed
+     */
     public function provide($key)
     {
         return $this->get($key)->provide($this);
     }
 
+    /**
+     * Calls provide for each key.
+     *
+     * @param string[] $keys
+     * @return mixed[]
+     */
     public function provideAll(array $keys)
     {
         $values = [];
@@ -42,26 +77,58 @@ class Slinpin implements InjectableDependencyContainer
         return $values;
     }
 
+    /**
+     * Injects dependencies into a class specified by $class_name.
+     *
+     * @param string $class_name
+     * @return object
+     */
     public function instance($class_name)
     {
         return $this->reflection_container->instance($class_name, $this);
     }
 
+    /**
+     * Invokes and injects dependencies into a method on an object, then returns the result.
+     *
+     * @param object $object
+     * @param string $method_name
+     * @return mixed
+     */
     public function invoke($object, $method_name)
     {
         return $this->reflection_container->invoke($object, $method_name, $this);
     }
 
+    /**
+     * Invokes and injects dependencies into a callback, then returns the result.
+     *
+     * @param callable $callback
+     * @return mixed
+     */
     public function call(callable $callback)
     {
         return $this->reflection_container->call($callback, $this);
     }
 
+    /**
+     * Registers a value provider.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
     public function value($key, $value)
     {
         $this->set($key, new ValueProvider($value));
     }
 
+    /**
+     * Registers a new factory provider.
+     *
+     * @param string $key
+     * @param string $class_name
+     * @param bool $cache
+     */
     public function factory($key, $class_name, $cache = true)
     {
         $provider = new FactoryProvider($class_name, $this->reflection_container);
@@ -73,6 +140,13 @@ class Slinpin implements InjectableDependencyContainer
         $this->set($key, $provider);
     }
 
+    /**
+     * Registers a callback provider.
+     *
+     * @param string $key
+     * @param callable $callback
+     * @param bool $cache
+     */
     public function callback($key, callable $callback, $cache = true)
     {
         $provider = new CallbackProvider($callback, $this->reflection_container);
